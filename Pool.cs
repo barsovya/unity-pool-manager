c850804aeda7bae8f95f9d71c00999347faa0dee
barsovya <68317444+barsovya@users.noreply.github.com>
2023-04-01 21:38:06 +0300

    init
    The implementation is written for the Unity engine

---
+++ Pool.cs
@@ -0,0 +1,150 @@
+﻿#define AdditionalValidation//checks was instance given when it tries to return
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum PoolEmptyBehaviour
+{
+    AllocateMore,
+    ReturnNull,
+    ReturnCyclicallyAlreadyGiven
+}
+
+[RequireComponent(typeof(PoolManager))]
+public class Pool : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int CountToAllocateInStart = 10;
+    public Queue<GameObject> InsideInstances;
+    public LinkedList<GameObject> OutsideInstances = new LinkedList<GameObject>();
+
+    public bool ChangeActiveState = true;//enable and disable objects when it comes in or out pool.
+    public bool ChangeParent = false;//enable and disable objects when it comes in or out pool.
+
+    public PoolEmptyBehaviour EmptyBehaviour = PoolEmptyBehaviour.AllocateMore;
+    public int CountToAllocateIfReachedLimit = 1;
+
+    public GameObjectEvent InstanceTaking = new GameObjectEvent();
+    public GameObjectEvent InstanceTaken = new GameObjectEvent();
+    public GameObjectEvent InstanceReturning = new GameObjectEvent();
+    public GameObjectEvent InstanceReturned = new GameObjectEvent();
+
+    public string InternInstanceName { get; private set; }
+    private bool _isInitialized = false;
+
+    private void OnValidate()
+    {
+        if (CountToAllocateInStart < 0)
+            CountToAllocateInStart = 0;
+
+        if (CountToAllocateIfReachedLimit < 0)
+            CountToAllocateIfReachedLimit = 1;
+    }
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    public string Initialize()
+    {
+        if (!_isInitialized)
+        {
+            OnValidate();
+            InternInstanceName = string.Intern(Prefab.name);
+            InsideInstances = new Queue<GameObject>(CountToAllocateInStart);
+            Allocate(CountToAllocateInStart);
+            _isInitialized = true;
+        }
+
+        return InternInstanceName;
+    }
+
+    public void Allocate(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            var instance = Instantiate(Prefab, transform);
+            instance.name = InternInstanceName;
+
+#if AdditionalValidation
+            OutsideInstances.AddLast(instance);
+#endif
+
+            ReturnToPool(instance);
+        }
+    }
+
+    public void ReturnToPool(GameObject instance)
+    {
+#if AdditionalValidation
+        if (!OutsideInstances.Contains(instance))
+        {
+            if (!InsideInstances.Contains(instance))
+                Debug.LogException(new ArgumentException($"This instance '{instance.name}' was not given, but tries to return."), instance);
+
+            return;
+        }
+        OutsideInstances.Remove(instance);
+#endif
+        InstanceReturning.Invoke(instance);
+
+        if (ChangeActiveState)
+            instance.SetActive(false);
+
+        if (ChangeParent)
+            instance.transform.SetParent(transform);
+
+        InsideInstances.Enqueue(instance);
+        InstanceReturned.Invoke(instance);
+    }
+
+    public GameObject TakeFromPool()
+    {
+        GameObject res = null;
+
+        if (InsideInstances.Count == 0)
+        {
+            switch (EmptyBehaviour)
+            {
+                case PoolEmptyBehaviour.AllocateMore:
+                    Allocate(CountToAllocateIfReachedLimit);
+                    res = InsideInstances.Dequeue();
+                    break;
+
+                case PoolEmptyBehaviour.ReturnCyclicallyAlreadyGiven:
+                    var outsideOne = OutsideInstances.First.Value;
+#if !AdditionalValidation
+                    OutsideInstances.RemoveFirst();
+#endif
+                    ReturnToPool(outsideOne);
+                    res = InsideInstances.Dequeue();
+                    break;
+
+                case PoolEmptyBehaviour.ReturnNull:
+                    return res;
+
+                default:
+                    throw new ArgumentException(nameof(EmptyBehaviour));
+            }
+        }
+        else
+        {
+            res = InsideInstances.Dequeue();
+        }
+
+        InstanceTaking.Invoke(res);
+
+        if (ChangeParent)
+            res.transform.SetParent(null);
+
+        if (ChangeActiveState)
+            res.SetActive(true);
+
+        OutsideInstances.AddLast(res);
+        InstanceTaken.Invoke(res);
+
+        return res;
+    }
+}
---
+++ PoolManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PoolManager : MonoBehaviour
+{
+    private static PoolManager _instance;
+
+    private Dictionary<string, Pool> _pools = new Dictionary<string, Pool>();
+
+    private void Awake()
+    {
+        foreach (var pool in GetComponents<Pool>())
+        {
+            _pools.Add(pool.Initialize(), pool);
+        }
+        _instance = this;
+    }
+
+    /// <summary>
+    /// Check does specific object have a pool responsible for it's instansing.
+    /// </summary>
+    /// <param name="obj">Instance or prefab</param>
+    /// <returns></returns>
+    public static bool HasPoolForObject(GameObject obj)
+    {
+        return _instance._pools.ContainsKey(obj.name);
+    }
+
+    public static GameObject Instantiate(string prefab, Vector3 position, Quaternion rotation)
+    {
+#if AllowNonPooledPrefabs
+        if (!_instance._pools.ContainsKey(prefab))
+        {
+            Debug.Log($"No such prefab with this name '{prefab}' in pool. Doing GameObject.Instantiate().");
+            return null;
+        }
+#endif
+
+        var instance = _instance._pools[prefab].TakeFromPool();
+#if AdditionalValidation
+        instance.AddComponent<PoolInstantiated>().Stack = new System.Diagnostics.StackTrace().ToString();
+#endif
+        instance.transform.SetPositionAndRotation(position, rotation);
+        return instance;
+    }
+
+    public static GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent)
+    {
+#if AllowNonPooledPrefabs
+        if (!HasPoolForObject(prefab))
+        {
+            Debug.Log($"No such prefab with this name '{prefab.name}' in pool. Doing GameObject.Instantiate().");
+            return GameObject.Instantiate(prefab, position, rotation, parent);
+        }
+#endif
+
+        var instance = _instance._pools[prefab.name].TakeFromPool();
+#if AdditionalValidation
+        instance.AddComponent<PoolInstantiated>().Stack = new System.Diagnostics.StackTrace().ToString();
+#endif
+        instance.transform.SetPositionAndRotation(position, rotation);
+        instance.transform.SetParent(parent);
+        return instance;
+    }
+
+    public static GameObject Instantiate(GameObject prefab)
+    {
+#if AllowNonPooledPrefabs
+        if (!HasPoolForObject(prefab))
+        {
+            Debug.Log($"No such prefab with this name '{prefab.name}' in pool. Doing GameObject.Instantiate().");
+            return GameObject.Instantiate(prefab);
+        }
+#endif
+
+        var instance = _instance._pools[prefab.name].TakeFromPool();
+#if AdditionalValidation
+        instance.AddComponent<PoolInstantiated>().Stack = new System.Diagnostics.StackTrace().ToString();
+#endif
+        return instance;
+    }
+
+    public static GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+#if AllowNonPooledPrefabs
+        if (!HasPoolForObject(prefab))
+        {
+            Debug.Log($"No such prefab with this name '{prefab.name}' in pool. Doing GameObject.Instantiate().");
+            return GameObject.Instantiate(prefab, position, rotation);
+        }
+#endif
+
+        var instance = _instance._pools[prefab.name].TakeFromPool();
+#if AdditionalValidation
+        instance.AddComponent<PoolInstantiated>().Stack = new System.Diagnostics.StackTrace().ToString();
+#endif
+        instance.transform.SetPositionAndRotation(position, rotation);
+        return instance;
+    }
+
+    public static void Destroy(GameObject instance)
+    {
+#if AllowNonPooledPrefabs
+        if (!HasPoolForObject(instance))
+        {
+            Debug.Log($"No such prefab with this name '{instance.name}' in pool. Doing GameObject.Destroy().");
+            GameObject.Destroy(instance);
+            return;
+        }
+#endif
+
+#if AdditionalValidation
+        instance.AddComponent<PoolDestroyed>().Stack = new System.Diagnostics.StackTrace().ToString();
+#endif
+        _instance._pools[instance.name].ReturnToPool(instance);
+    }
+
+    public static void Destroy(GameObject instance, float time)
+    {
+#if AllowNonPooledPrefabs
+        if (!HasPoolForObject(instance))
+        {
+            Debug.Log($"No such prefab with this name '{instance.name}' in pool. Doing GameObject.Destroy().");
+            GameObject.Destroy(instance, time);
+            return;
+        }
+#endif
+
+#if AdditionalValidation
+        instance.AddComponent<PoolDestroyed>().Stack = new System.Diagnostics.StackTrace().ToString();
+#endif
+        _instance.StartCoroutine(DelayedDestroy(instance, time));
+    }
+
+    private static IEnumerator DelayedDestroy(GameObject instance, float time)
+    {
+        yield return new WaitForSeconds(time);
+        _instance._pools[instance.name].ReturnToPool(instance);
+    }
+}
